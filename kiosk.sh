29502676b7e0df9e7e0b7c5d4759427d5cd4b599
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-08-29 21:44:45 -0700

    Added prefix to start script

--- kiosk.sh
+++ kiosk.sh
@@ -4,7 +4,7 @@ export CHROME_PATH="/usr/bin/chromium-browser"
 xset -dpms     # disable DPMS (Energy Star) features.
 xset s off     # disable screen saver
 xset s noblank # don't blank the video device
-npm start &
+npm --prefix /home/pi/talkinhead start &
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-npm run kiosk
+npm --prefix /home/pi/talkinghead run kiosk
