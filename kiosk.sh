d432ac912377934b337796757dacac726960661c
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-08-29 19:15:05 -0700

    shell script to handle startup commands

---
+++ kiosk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export CHROME_PATH="/usr/bin/chromium-browser"
+xset -dpms     # disable DPMS (Energy Star) features.
+xset s off     # disable screen saver
+xset s noblank # don't blank the video device
+npm start &
+matchbox-window-manager -use_titlebar no &
+unclutter &    # hide X mouse cursor unless mouse activated
+npm run kiosk
