cf880f9ac0d74115d26ea0a59f5e815175feea17
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-03 00:32:35 -0700

    removrd festival from start script

--- kiosk.sh
+++ kiosk.sh
@@ -7,5 +7,4 @@ xset s off     # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-sudo festival --server '(set! server_port 1515)' &
 npm --prefix /home/pi/talkinghead start > /home/pi/log-file.txt 2> /home/pi/error-file.txt
