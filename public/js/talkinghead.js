9c82b68c3b8f05fa94902d8532d5574c2b030e88
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-02 17:29:45 -0700

    using mbrola voices

--- public/js/talkinghead.js
+++ public/js/talkinghead.js
@@ -64,18 +64,18 @@ function startConversation() {
         left = new SpeechSynthesisUtterance(conversation.parent);
         left.volume = volume;
         console.log(volume);
-        left.voice = voices.filter(function(voice) { return voice.name.includes('mb-us1'); })[0];
+        left.voice = voices.filter(function(voice) { return voice.name.includes('us1'); })[0];
         left.pitch = 0.9;
-        left.rate = 0.7;
+        left.rate = 0.8;
 
         left.onend = function(e) {
             toggleTalkingAnimation(woman, false);
             right = new SpeechSynthesisUtterance(conversation.comment);
             right.volume = volume;
             console.log(volume);
-            right.voice = voices.filter(function(voice) { return voice.name.includes('mb-us2'); })[0];
+            right.voice = voices.filter(function(voice) { return voice.name.includes('us2'); })[0];
             right.pitch = 0.9;
-            right.rate = 0.7;
+            right.rate = 0.8;
             synthesizer.speak(right);
 
             right.onend = function() {
