1a0e5e173b8afb9741956a14b3b17f66e2d1d764
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-02 19:56:39 -0700

    no longer waiting to start

--- kiosk.sh
+++ kiosk.sh
@@ -7,6 +7,4 @@ xset s off     # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-sleep 5s
-sudo killall /usr/bin/speech-dispatcher
 speech-dispatcher -d && npm --prefix /home/pi/talkinghead start > /home/pi/log-file.txt 2> /home/pi/error-file.txt
