48a429178026eb779b2f5e25e991e5c023b06583
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-02 20:27:47 -0700

    starting app without calling speech-dispatcher

--- kiosk.sh
+++ kiosk.sh
@@ -7,4 +7,4 @@ xset s off     # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-speech-dispatcher -d && npm --prefix /home/pi/talkinghead start > /home/pi/log-file.txt 2> /home/pi/error-file.txt
+npm --prefix /home/pi/talkinghead start > /home/pi/log-file.txt 2> /home/pi/error-file.txt
