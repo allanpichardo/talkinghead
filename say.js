ff29ef3412f788201a5396ef655df92923ad69ca
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-04 14:42:56 -0700

    using shell format

--- say.js
+++ say.js
@@ -3,27 +3,21 @@ const WebSocket = require('ws');
 
 function say(text, voice, client) {
   console.log(`Spawning voxin with voice ${voice}`);
-  const voxin = spawn(`voxin-say`, ['-l', voice, `"${text}"`]);
-  voxin.stdout.on('data', data => {
+  const child = spawn(`voxin-say -l ${voice} "${text}" | aplay`, {shell: true});
+  child.stdout.on('data', data => {
     console.log(`Voxin is ouptutting data`);
-    console.log(`Spawning aplay`);
-    const aplay = spawn('aplay');
-
-    console.log(`Writing data`);
-    aplay.stdin.write(data);
-    aplay.on('close', () => {
-      console.log(`aplay closed`);
-
-      const reply = {};
-      reply.route = 'speech-end';
-      reply.voice = voice;
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(reply));
-      }
-    });
-
     const reply = {};
     reply.route = 'speech-start';
+    reply.voice = voice;
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(reply));
+    }
+  });
+
+  child.on('close', () => {
+    console.log(`aplay closed`);
+    const reply = {};
+    reply.route = 'speech-end';
     reply.voice = voice;
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(reply));
