76193902fa447508eab929d27a8bb630e511a4b0
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-04 15:46:57 -0700

    testing voxin out to stdout

--- say.js
+++ say.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 
 function say(text, voice, client) {
   console.log(`Spawning voxin with voice ${voice}`);
-  const child = spawn(`voxin-say -j 2 -l ${voice} "${text}" | aplay`, {shell: true, });
+  const child = spawn(`voxin-say -j 2 -l ${voice} "${text}"`, {shell: true, });
 
   child.on('close', () => {
     console.log(`aplay closed`);
@@ -13,6 +13,10 @@ function say(text, voice, client) {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(reply));
     }
+  });
+
+  child.stdout.on('data', data => {
+    console.log(data);
   });
 
   setTimeout(() => {
