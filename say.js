9079156d2292ceb706109171c90ca5580896eb41
Allan Pichardo <allan.pichardo@whoisowenjones.com>
2021-10-04 14:37:46 -0700

    added verbose logging

--- say.js
+++ say.js
@@ -2,12 +2,18 @@ const {spawn} = require('child_process');
 const WebSocket = require('ws');
 
 function say(text, voice, client) {
+  console.log(`Spawning voxin with voice ${voice}`);
   const voxin = spawn(`voxin-say`, ['-l', voice, `"${text}"`]);
-
   voxin.stdout.on('data', data => {
+    console.log(`Voxin is ouptutting data`);
+    console.log(`Spawning aplay`);
     const aplay = spawn('aplay');
+
+    console.log(`Writing data`);
     aplay.stdin.write(data);
     aplay.on('close', () => {
+      console.log(`aplay closed`);
+
       const reply = {};
       reply.route = 'speech-end';
       reply.voice = voice;
